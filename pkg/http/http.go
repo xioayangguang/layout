95d321794b24c97139f7bdbef32185889fd57593
DESKTOP-071RMTM\Administrator <930029203@qq.com>
2023-07-20 22:18:15 +0800

    添加注释

--- pkg/http/http.go
+++ pkg/http/http.go
@@ -13,9 +13,16 @@ import (
 
 func Run(r *gin.Engine, addr string) {
 
-	//if err := endless.ListenAndServe(":8080", router); err!=nil{
-	//	log.Fatalf("listen: %s\n", err)
-	//}
+	// 默认endless服务器会监听下列信号：
+	// syscall.SIGHUP，syscall.SIGUSR1，syscall.SIGUSR2，syscall.SIGINT，syscall.SIGTERM和syscall.SIGTSTP
+	// 接收到 SIGHUP 信号将触发`fork/restart` 实现优雅重启（kill -1 pid会发送SIGHUP信号）
+	// 接收到 syscall.SIGINT或syscall.SIGTERM 信号将触发优雅关机
+	// 接收到 SIGUSR2 信号将触发HammerTime
+	// SIGUSR1 和 SIGTSTP 被用来触发一些用户自定义的hook函数
+	//执行kill -1 pid命令发送syscall.SIGINT来通知程序优雅重启，具体做法如下：
+	if err := endless.ListenAndServe(":8080", router); err != nil {
+		log.Fatalf("listen: %s\n", err)
+	}
 
 	srv := &http.Server{
 		Addr:    addr,
