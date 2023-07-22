c53ef212410813a5d65f684e538fe0db2e8dbaa7
DESKTOP-071RMTM\Administrator <930029203@qq.com>
2023-07-22 19:52:29 +0800

    添加注释

--- internal/router/http.go
+++ internal/router/http.go
@@ -2,16 +2,25 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"layout/global"
 	"layout/internal/handler"
 	"layout/internal/router/api"
 	"layout/internal/router/h5"
+	"layout/pkg/helper/rotatelogs"
 )
 
 func NewServerHTTP(
 	userHandler handler.UserHandler,
 ) *gin.Engine {
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
+	var r *gin.Engine
+	if !global.Config.Debug {
+		gin.SetMode(gin.ReleaseMode)
+		r = gin.New()
+		r.Use(gin.LoggerWithWriter(rotatelogs.GetRotateLogs("output")), gin.RecoveryWithWriter(rotatelogs.GetRotateLogs("recovery")))
+	} else {
+		r = gin.Default()
+	}
+
 	api.InitApiRouter(r, userHandler)
 	h5.InitApiRouter(r, userHandler)
 	InitApiRouter(r, userHandler)
