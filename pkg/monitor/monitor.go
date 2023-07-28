a0d91ea8aa0732dd17bcb2a57d87b8af326fa3cd
小阳光 <930029203@qq.com>
2023-07-28 14:41:28 +0800

    Update monitor.go
    修改监控

--- pkg/monitor/monitor.go
+++ pkg/monitor/monitor.go
@@ -29,33 +29,46 @@ func init() {
 				continue
 			}
 			if monitorState {
-				
 				if v.UsedPercent >= 80 || cpuPercent[0] >= 80 {
-					//if true {
-					cpuFile, err = os.OpenFile(fmt.Sprintf("./log/pprof/%s-cpu%v.pprof", time.Now().Format("01-02-15-4-5"), int(cpuPercent[0])), os.O_CREATE, 0666)
+					cpuFile, err = os.OpenFile(fmt.Sprintf("./log/pprof/%s-cpu%v.pprof", time.Now().Format("01-02-15-4-5"), int(cpuPercent[0])), os.O_CREATE|os.O_WRONLY, 0666)
 					if err != nil {
+						fmt.Println(err)
 						return
 					}
-					_ = pprof.StartCPUProfile(cpuFile)
-					memFile, err = os.OpenFile(fmt.Sprintf("./log/pprof/%s-mem%v.pprof", time.Now().Format("01-02-15-4-5"), int(v.UsedPercent)), os.O_CREATE, 0666)
+					err = pprof.StartCPUProfile(cpuFile)
 					if err != nil {
+						fmt.Println(err)
+					}
+					memFile, err = os.OpenFile(fmt.Sprintf("./log/pprof/%s-mem%v.pprof", time.Now().Format("01-02-15-4-5"), int(v.UsedPercent)), os.O_CREATE|os.O_WRONLY, 0666)
+					if err != nil {
+						fmt.Println(err)
 						return
 					}
-					_ = pprof.WriteHeapProfile(memFile)
+					err = pprof.WriteHeapProfile(memFile)
+					if err != nil {
+						fmt.Println(err)
+					}
 					monitorStartTime = time.Now().Unix()
 					monitorState = false
-				
+					fmt.Println("开始")
 				}
 			}
 			if !monitorState {
-				//if (v.UsedPercent < 80 && cpuPercent[0] < 80) || (monitorStartTime+30 < time.Now().Unix()) {
-				if monitorStartTime+30 < time.Now().Unix() {
+				if (v.UsedPercent < 80 && cpuPercent[0] < 80) || (monitorStartTime+30 < time.Now().Unix()) {
 					pprof.StopCPUProfile()
 					monitorStartTime = 0
 					monitorState = true
-					_ = cpuFile.Close()
-					_ = memFile.Close()
-					
+					err = cpuFile.Close()
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					err = memFile.Close()
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					fmt.Println("结束")
 				}
 			}
 		}
