8c935865043428fef54192ffc38c803e6f592907
小阳光 <930029203@qq.com>
2023-07-28 10:55:04 +0800

    Update monitor.go
    修改监控

--- pkg/monitor/monitor.go
+++ pkg/monitor/monitor.go
@@ -14,6 +14,8 @@ func init() {
 		_ = os.MkdirAll("./log/pprof/", os.ModePerm)
 		var monitorStartTime int64 = 0
 		var monitorState = true
+		var cpuFile *os.File
+		var memFile *os.File
 		interval := time.Second * 5
 		ticker := time.NewTicker(interval)
 		defer ticker.Stop()
@@ -27,28 +29,33 @@ func init() {
 				continue
 			}
 			if monitorState {
+				
 				if v.UsedPercent >= 80 || cpuPercent[0] >= 80 {
-					cpuFile, err := os.OpenFile(fmt.Sprintf("./log/pprof/%s-cpu%v.pprof", time.Now().Format("01-02-15-4-5"), int(cpuPercent[0])), os.O_CREATE, 0666)
+					//if true {
+					cpuFile, err = os.OpenFile(fmt.Sprintf("./log/pprof/%s-cpu%v.pprof", time.Now().Format("01-02-15-4-5"), int(cpuPercent[0])), os.O_CREATE, 0666)
 					if err != nil {
 						return
 					}
 					_ = pprof.StartCPUProfile(cpuFile)
-					memFile, err := os.OpenFile(fmt.Sprintf("./log/pprof/%s-mem%v.pprof", time.Now().Format("01-02-15-4-5"), v.UsedPercent), os.O_CREATE, 0666)
+					memFile, err = os.OpenFile(fmt.Sprintf("./log/pprof/%s-mem%v.pprof", time.Now().Format("01-02-15-4-5"), int(v.UsedPercent)), os.O_CREATE, 0666)
 					if err != nil {
 						return
 					}
 					_ = pprof.WriteHeapProfile(memFile)
-					_ = cpuFile.Close()
-					_ = memFile.Close()
 					monitorStartTime = time.Now().Unix()
 					monitorState = false
+				
 				}
 			}
 			if !monitorState {
-				if (v.UsedPercent < 80 && cpuPercent[0] < 80) || (monitorStartTime+30 < time.Now().Unix()) {
+				//if (v.UsedPercent < 80 && cpuPercent[0] < 80) || (monitorStartTime+30 < time.Now().Unix()) {
+				if monitorStartTime+30 < time.Now().Unix() {
 					pprof.StopCPUProfile()
 					monitorStartTime = 0
 					monitorState = true
+					_ = cpuFile.Close()
+					_ = memFile.Close()
+					
 				}
 			}
 		}
